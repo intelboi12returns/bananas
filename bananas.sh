8d2af30a3bd7083a1e85a64ed4581ebc9a7e5f0c
intelboi12returns <intelboiduabelas@gmail.com>
2025-11-30 23:49:04 +0800

    Implement continuous banana lore and options display
    Added a loop to continuously display banana-related options and lore.

--- bananas.sh
+++ bananas.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+while true; do
 sleep 2
 
 figlet BANANAS
@@ -13,9 +14,11 @@ echo "So all you gotta do is to choose between..."
 echo " "
 echo "[R] Read the lore of bananas!"
 sleep 1
+echo "[I] See some pictures of bananas!"
+sleep 1
 echo "Or..."
 sleep 1
-echo "[I] See some pictures of bananas!"
+echo "[Q] Quit"
 read choice
 
 case $choice in
@@ -71,3 +74,31 @@ echo "Announcer? Why is there a new contestant??"
 echo "A tree!"
  echo "soon"
        ;;
+   esac
+done
+       ;;
+
+        R)
+echo "Let's learn about Bananas!"
+ sleep 2
+ echo "The wild banana plant is believed to have originated in Southeast Asia, with early ancestors like Musa acuminata and Musa ornata found in regions from South India to Cambodia and Sumatra.
+Over time, these plants were cultivated and developed into the edible, seedless bananas enjoyed today, a testament to human and natural selection."
+        
+        More)
+ echo "Why bananas belong to monkeys: A traditional Brazilian story tells of a woman who raised bananas and was frustrated by monkeys stealing them. One day, she captured a monkey, but as it escaped, its body became the first banana, and all bananas have been part of the monkeys' inheritance ever since, says Storynory.
+The legend of the banana: A similar legend from the 19th century tells of a family that lived in a jungle and were poor. One day, a woman found a magical, singing banana in the jungle, which led to the discovery of a hidden treasure. The story of the banana is used to show how even the most common things can be magical if we just look closer.
+Living in the present: A modern story uses the banana as a metaphor for living in the moment. It tells of a wise monk who advises his disciple to enjoy the present instead of waiting for the future, reminding him that the ripened banana is already in his hands,"
+        ;;
+
+        Q)
+echo "See you later!"
+   sleep 2
+   exit 1
+        ;;
+esac
+
+ echo " "
+
+exit 0
+
+done        
